f1db4442390e51a56f58fc96b0b98ab1033fff1b
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2026-04-13 16:37:03 +0530

    feat: LoginForm Created

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,13 @@
+const LoginForm = () => {
+    return (
+        <div>
+            <form>
+                <label htmlFor="username">Username</label>
+                <input name="username" />
+            </form>
+        </div>
+    )
+}
+
+export default LoginForm;
+
